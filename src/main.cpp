72b3284e5132da8c723db0d65c869843e596a434
unknown <prox918@gmail.com>
2025-01-21 22:04:12 +0300

    remove pressets from git ignore

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #include "Vector.hpp"
 
 int main() {
-    constexpr static Vector<int, 4> vec1(1.1, 3, 4, 5);
+    constexpr static Vector<int, 4> vec1(1, 3, 4, 5);
     constexpr static Vector<int, 4> vec2(1, 5, 4, 3);
 
     //auto scalar = vec1 ^ vec2;
